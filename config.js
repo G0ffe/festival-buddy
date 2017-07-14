6f16fa45d999a2ec15ab8f0eba0ef203239b1e27
G0ffe <djg0ffe@gmail.com>
2017-07-14 20:15:38 +0200

    heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/festival-app';
+                      'mongodb://g0ffe:test1234@ds157682.mlab.com:57682/festivals';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-festival-app');
