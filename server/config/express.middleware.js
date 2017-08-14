e25f465c6813570d8ceefedd800ac0fd28450025
G0ffe <djg0ffe@gmail.com>
2017-08-14 20:56:21 +0200

    done

--- server/config/express.middleware.js
+++ server/config/express.middleware.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 module.exports = function middleware(app) {
-  app.use(function(req, res, next) {
+  /* app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-   });
+   }); */
   mongoose.Promise = global.Promise;
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
